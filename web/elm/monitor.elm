f7cde700ebb37c7aaa9a4344345de006791a22ba
Rickard Andersson <gonz@severnatazvezda.com>
2016-04-02 22:36:53 +0300

    Add auto-updating monitor list

--- web/elm/monitor.elm
+++ web/elm/monitor.elm
@@ -3,51 +3,77 @@ module Monitor where
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Html.Events exposing (..)
-import StartApp.Simple exposing (start)
+import StartApp exposing (start)
 import String exposing (toLower)
+import Effects exposing (Effects, Never)
+import Task exposing (Task)
+import Json.Decode as Json exposing ((:=))
 
 -- Model
-type Status
-  = Up
-  | Down
-
-type Kind
-  = Node
-  | Host
-  | Process
-
-type alias Monitoree = {kind : Kind, name : String, status : Status}
+type alias Monitoree = {kind : String, name : String, status : String}
 
 type alias Model = List Monitoree
 
-initialModel : Model
-initialModel = [{kind = Node, name = "node", status = Up}]
+init : (Model, Effects Action)
+init =
+  ([], Effects.none)
+  
 
 type Action
   = NoOp
-  | Update
+  | SetMonitorees Model
 
 -- Update
-update : Action -> Model -> Model
+update : Action -> Model -> (Model, Effects Action)
 update action model =
   case action of
     NoOp ->
-      initialModel
-    _ ->
-      initialModel
+      ([], Effects.none)
+    SetMonitorees monitorees ->
+      (monitorees, Effects.none)
 
 -- View
 renderMonitoree : Monitoree -> Html
 renderMonitoree monitoree =
-  let monName = monitoree.name
-      monKind = monitoree.kind |> toString |> toLower
-      monStatus = monitoree.status |> toString |> toLower
+  let name = monitoree.name
+      kind = monitoree.kind
+      status = monitoree.status
   in
-  span [] [text (monName ++ " (" ++ monKind ++ ") is " ++ monStatus)]
+  div [] [text (name ++ " (" ++ kind ++ ") is " ++ status)]
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   div [] (List.map renderMonitoree model)
 
+-- Effects
+decodeMonitorees : Json.Decoder Model
+decodeMonitorees =
+  let
+    monitoree =
+      Json.object3 (\kind name status -> (Monitoree kind name status))
+          ("kind" := Json.string)
+          ("name" := Json.string)
+          ("status" := Json.string)
+  in
+    Json.at ["data"] (Json.list monitoree)
+
+-- Signals
+incomingActions : Signal Action
+incomingActions =
+  Signal.map SetMonitorees monitorees
+
+port tasks : Signal (Task Never ())
+port tasks =
+  app.tasks
+
+port monitorees : Signal Model
+
+app = start { init = init
+            , inputs = [incomingActions]
+            , update = update
+            , view = view
+            }
+
+main : Signal Html
 main =
-  start {model = initialModel, update = update, view = view}
+  app.html
